471628ea347ec1a8a9946c34e765942586207dbf
unknown <piotr.peter@rad4m.com>
2020-10-23 12:31:32 +0200

    lab3 integration test

--- src/test/agh/cs/lab3/AnimalWalkingTest.java
+++ src/test/agh/cs/lab3/AnimalWalkingTest.java
@@ -1,11 +1,45 @@
 package agh.cs.lab3;
 
-import agh.cs.lab2.World;
+import agh.cs.lab2.MapDirection;
+import agh.cs.lab2.MoveDirection;
+import agh.cs.lab2.Vector2d;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AnimalWalkingTest {
-    void test () {
 
+    @Test
+    @DisplayName("Whole animal walking process")
+    void test() {
+        Animal testAnimal = new Animal();
+        testAnimal.setPosition(new Vector2d(2, 2));
+        testAnimal.setOrientation(MapDirection.NORTH);
+        Vector2d expectedFinalPosition = new Vector2d(2, 2);
+
+        // args are parsed correctly and animal moves as expected
+        String[] mockArgs = {
+                "l", "forward", "wrongOne", "r",
+                "backward", "unexpectedOne", "b",
+                "right", "f", "left",
+        };
+        List<MoveDirection> directions =  OptionsParser.parse(mockArgs);
+        for (MoveDirection direction : directions) {
+            testAnimal.move(direction);
+        }
+        expectedFinalPosition = new Vector2d(2,0);
+        assertTrue(testAnimal.getOrientation() == MapDirection.NORTH);
+        assertTrue(testAnimal.getPosition().equals(expectedFinalPosition));
+
+        // animal doesn't move outside of the board
+        mockArgs = new String[] {"b", "backward"};
+        directions =  OptionsParser.parse(mockArgs);
+        for (MoveDirection direction : directions) {
+            testAnimal.move(direction);
+        }
+        expectedFinalPosition = new Vector2d(2,0);
+        assertTrue(testAnimal.getPosition().equals(expectedFinalPosition));
     }
 }
